1f4371cee6883c76ac0c5305ddbc3ab192e1b7cc
Dominik Engelmann <dengelma.solutions@gmail.com>
2025-01-05 10:46:36 +0100

    add logging messages to all controller methods

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException, Logger } from '@nestjs/common';
 import { CitiesService } from './cities.service';
 import { City } from './interfaces/city.interface';
 import { Chatroom } from './interfaces/chatroom.interface';
@@ -7,15 +7,19 @@ import { Message } from './interfaces/message.interface';
 
 @Controller('cities')
 export class CitiesController {
+  private readonly logger = new Logger(CitiesController.name);
+
   constructor(private readonly citiesService: CitiesService) {}
 
   @Get()
   public async getAll(): Promise<City[]> {
+    this.logger.log('GET /cities');
     return this.citiesService.getAll();
   }
 
   @Get(':id')
   public async getById(@Param('id') id: string): Promise<City> {
+    this.logger.log(`GET /cities/${id}`);
     const city = await this.citiesService.getById(id);
     if (!city) {
       throw new NotFoundException('City not found');
@@ -23,49 +27,31 @@ export class CitiesController {
     return city;
   }
 
-  @Get(':id/:subcollection')
-  public async getSubcollection(
-    @Param('id') id: string,
-    @Param('subcollection') subcollection: string,
-  ): Promise<any[]> {
-    return this.citiesService.getSubcollectionData(id, subcollection);
-  }
 
-  @Get('subcollections/:name')
-  public async getAllFromSubcollection(@Param('name') name: string): Promise<any[]> {
-    return this.citiesService.queryAllSubcollections(name);
-  }
 
   @Get(':id/chatrooms')
   public async getCityChatrooms(@Param('id') id: string): Promise<Chatroom[]> {
+    this.logger.log(`GET /cities/${id}/chatrooms`);
     return this.citiesService.getChatrooms(id);
   }
 
-  @Get('chatrooms/all')
-  public async getAllChatrooms(): Promise<Chatroom[]> {
-    return this.citiesService.getAllChatrooms();
-  }
 
   @Get(':id/events')
   public async getCityEvents(@Param('id') id: string): Promise<Event[]> {
+    this.logger.log(`GET /cities/${id}/events`);
     return this.citiesService.getEvents(id);
   }
 
-  @Get('events/all')
-  public async getAllEvents(): Promise<Event[]> {
-    return this.citiesService.getAllEvents();
-  }
+
 
   @Get(':cityId/chatrooms/:chatroomId/messages')
   public async getChatroomMessages(
     @Param('cityId') cityId: string,
     @Param('chatroomId') chatroomId: string,
   ): Promise<Message[]> {
+    this.logger.log(`GET /cities/${cityId}/chatrooms/${chatroomId}/messages`);
     return this.citiesService.getChatroomMessages(cityId, chatroomId);
   }
 
-  @Get('messages/all')
-  public async getAllMessages(): Promise<Message[]> {
-    return this.citiesService.getAllMessages();
-  }
+
 } 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,23 @@
-import { Controller, Get, Post, Put, Delete, Body, Param, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, NotFoundException, Logger } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserProfile } from './interfaces/user-profile.interface';
 import { UserProfileDto } from './dto/user-profile.dto';
 
 @Controller('users')
 export class UsersController {
+  private readonly logger = new Logger(UsersController.name);
+
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
   public async getAll(): Promise<UserProfile[]> {
+    this.logger.log('GET /users');
     return this.usersService.getAll();
   }
 
   @Get(':id')
   public async getById(@Param('id') id: string): Promise<UserProfile> {
+    this.logger.log(`GET /users/${id}`);
     const user = await this.usersService.getById(id);
     if (!user) {
       throw new NotFoundException('User not found');
@@ -26,6 +30,7 @@ export class UsersController {
     @Param('id') id: string,
     @Body() userProfileDto: UserProfileDto
   ): Promise<UserProfile> {
+    this.logger.log(`POST /users/${id}`);
     const { id: _, ...profile } = userProfileDto;
     return this.usersService.create(id, profile);
   }
@@ -35,11 +40,13 @@ export class UsersController {
     @Param('id') id: string,
     @Body() userProfileDto: Partial<UserProfileDto>
   ): Promise<UserProfile> {
+    this.logger.log(`PUT /users/${id}`);
     return this.usersService.update(id, userProfileDto);
   }
 
   @Delete(':id')
   public async delete(@Param('id') id: string): Promise<void> {
+    this.logger.log(`DELETE /users/${id}`);
     return this.usersService.delete(id);
   }
 } 
