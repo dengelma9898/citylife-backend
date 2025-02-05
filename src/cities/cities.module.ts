857f005d56a2a4583b8466906402307b13b352d0
Dominik Engelmann <dengelma.solutions@gmail.com>
2025-02-05 09:56:25 +0100

    fix cities module

--- src/cities/cities.module.ts
+++ src/cities/cities.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CitiesController } from './cities.controller';
-import { CurrentCityController } from './current-city.controller';
 import { CitiesService } from './cities.service';
-import { CurrentCityService } from './current-city.service';
 import { FirebaseModule } from '../firebase/firebase.module';
-import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [FirebaseModule, UsersModule],
-  controllers: [CitiesController, CurrentCityController],
-  providers: [CitiesService, CurrentCityService],
+  imports: [FirebaseModule],
+  controllers: [CitiesController],
+  providers: [CitiesService],
+  exports: [CitiesService],
 })
 export class CitiesModule {} 
