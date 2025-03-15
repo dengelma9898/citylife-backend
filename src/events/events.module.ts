1615c5d906ca108182fea4b1ebbd2c62af15eef9
Dominik Engelmann <dengelma.solutions@gmail.com>
2025-03-15 07:53:51 +0100

    add title image upload for event and add delete method for event

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 import { FirebaseModule } from '../firebase/firebase.module';
+import { FirebaseStorageService } from '../firebase/firebase-storage.service';
 
 @Module({
   imports: [FirebaseModule],
   controllers: [EventsController],
-  providers: [EventsService],
+  providers: [EventsService, FirebaseStorageService],
   exports: [EventsService]
 })
 export class EventsModule {} 
