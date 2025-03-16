038b99ac711b956be626fbba564f28b27901f5a6
Dominik Engelmann <dengelma.solutions@gmail.com>
2025-03-16 12:23:08 +0100

    add more contact data to business

--- src/businesses/interfaces/business-contact.interface.ts
+++ src/businesses/interfaces/business-contact.interface.ts
@@ -10,4 +10,24 @@ export interface BusinessContact {
    * Can be added by admin or business owner.
    */
   phoneNumber?: string;
+
+  /**
+   * Optional Instagram profile URL or username.
+   */
+  instagram?: string;
+
+  /**
+   * Optional Facebook page URL or username.
+   */
+  facebook?: string;
+
+  /**
+   * Optional TikTok profile URL or username.
+   */
+  tiktok?: string;
+
+  /**
+   * Optional website URL of the business.
+   */
+  website?: string;
 } 
