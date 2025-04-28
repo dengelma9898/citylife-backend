c1ffa08097ebec8ea7994705a00594366382f27c
Dominik Engelmann <dengelma.solutions@gmail.com>
2025-04-28 19:19:45 +0200

    add cors links depending on the stage

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   // Enable CORS
   app.enableCors({
     origin: [
-      'https://citylife-fe75a.web.app',  // Production Frontend
+      configService.get<string>('FRONTEND_URL'),
       'http://localhost:5173',           // Local Frontend (falls benötigt)
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
